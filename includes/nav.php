bb8def50051ad77b778154111e74f9e2f21266e8
Heini <heini.pollanen@samiedu365.fi>
2026-05-26 11:16:39 +0300

    Laitoin kuvan näkymään ja muokkasin nav-baria

--- includes/nav.php
+++ includes/nav.php
@@ -2,8 +2,9 @@
     <div class="brand">Our store</div>
 
 
-    <div class="links"></div>
-    <a data-active="index" href="index.php">Home</a>
-    <a data-active="about" href="about.php">About</a>
-    <a data-active="contact" href="contact.php">Contact</a>
+    <div class="links">
+        <a data-active="index" href="index.php">Home</a>
+        <a data-active="about" href="about.php">About</a>
+        <a data-active="contact" href="contact.php">Contact<a>
+    </div>
 </nav>
